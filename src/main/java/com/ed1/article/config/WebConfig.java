1847bdf3ca05253aef3777ebe7dbe07ec5684849
edmundo-vitor <edmundovitor1414@hotmail.com>
2022-05-15 11:06:16 -0300

    config: added cors config

---
+++ src/main/java/com/ed1/article/config/WebConfig.java
@@ -0,0 +1,41 @@
+package com.ed1.article.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	
+//	@Value("${upload.directory}")
+//	private String uploadDirectory;
+	
+	@Bean
+	public WebMvcConfigurer customConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	            configurer.defaultContentType(MediaType.APPLICATION_JSON);
+	        }
+	    };
+	}
+	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+    
+    @Override 
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+    	registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    	registry.addResourceHandler("/resources/**").addResourceLocations("file:" + uploadDirectory);
+    }
+}
