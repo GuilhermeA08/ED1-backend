567df15919f722c96e3f8f2758bff57a7cd67a90
edmundo-vitor <edmundovitor1414@hotmail.com>
2022-06-03 17:28:49 -0300

    feat: added attachment to article

--- src/main/java/com/ed1/article/controller/ArticleController.java
+++ src/main/java/com/ed1/article/controller/ArticleController.java
@@ -4,6 +4,8 @@ import com.ed1.article.model.Article;
 import com.ed1.article.service.ArticleService;
 import com.ed1.article.structures.List.DoubleLinkedList;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,53 +15,72 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/articles")
 public class ArticleController {
 
-  @Autowired
-  private ArticleService articleService;
+	@Autowired
+	private ArticleService articleService;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 
-  @GetMapping
-  public ResponseEntity<DoubleLinkedList<Article>> findAll() throws JsonProcessingException {
-    DoubleLinkedList<Article> articleList = articleService.findAll();
-    return ResponseEntity.ok().body(articleList);
-  }
+	@GetMapping
+	public ResponseEntity<DoubleLinkedList<Article>> findAll() throws JsonProcessingException {
+		DoubleLinkedList<Article> articleList = articleService.findAll();
+		return ResponseEntity.ok().body(articleList);
+	}
 
-  @GetMapping("/{id}")
-  public ResponseEntity<Article> findById(@PathVariable Long id) {
-    Article articleDto = articleService.findById(id);
-    return ResponseEntity.ok().body(articleDto);
-  }
+	@GetMapping("/{id}")
+	public ResponseEntity<Article> findById(@PathVariable Long id) {
+		Article articleDto = articleService.findById(id);
+		return ResponseEntity.ok().body(articleDto);
+	}
 
-  @PostMapping
-  public ResponseEntity<Article> save(@RequestBody Article article) {
-    Article articleDto = articleService.save(article);
-    URI uri = ServletUriComponentsBuilder
-      .fromCurrentRequest()
-      .path("/{id}")
-      .buildAndExpand(articleDto.getId())
-      .toUri();
-    return ResponseEntity.created(uri).body(articleDto);
-  }
+	@PostMapping
+	public ResponseEntity<Article> save(
+			@RequestParam(name = "article", required = true) String articleDTO,
+			@RequestParam(name = "attachment", required = false) MultipartFile attachment) throws JsonMappingException, JsonProcessingException {
+		
+		Article article = objectMapper.readValue(articleDTO, Article.class);
+		
+		Article articleDto = articleService.save(article, attachment);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(articleDto.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).body(articleDto);
+	}
 
-  @PutMapping("/{id}")
-  public ResponseEntity<Article> update(
-    @PathVariable Long id,
-    @RequestBody Article article
-  ) {
-    Article articleDto = articleService.update(id, article);
-    return ResponseEntity.ok().body(articleDto);
-  }
+	@PutMapping("/{id}")
+	public ResponseEntity<Article> update(
+			@PathVariable Long id,
+			@RequestParam(name = "article", required = true) String articleDTO,
+			@RequestParam(name = "attachment", required = false) MultipartFile attachment) throws JsonMappingException, JsonProcessingException {
+		
+		Article article = objectMapper.readValue(articleDTO, Article.class);
+		
+		Article articleDto = articleService.update(id, article, attachment);
+		return ResponseEntity.ok().body(articleDto);
+	}
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
-    articleService.deleteArticle(id);
-    return ResponseEntity.noContent().build();
-  }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+		articleService.deleteArticle(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}/attachment")
+	public ResponseEntity<Void> deleteArticleAttachment(@PathVariable Long id) {
+		articleService.deleteArticleAttachment(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/ed1/article/service/ArticleService.java
+++ src/main/java/com/ed1/article/service/ArticleService.java
@@ -5,6 +5,8 @@ import com.ed1.article.repository.ArticleRepository;
 import com.ed1.article.repository.RatingRepository;
 import com.ed1.article.repository.UserRepository;
 import com.ed1.article.structures.List.DoubleLinkedList;
+import com.ed1.article.util.FileUtil;
+
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -12,73 +14,111 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ArticleService {
 
-  @Autowired
-  private ArticleRepository articleRepository;
-  
-  @Autowired
-  private UserRepository userRepository;
-  
-  @Autowired
-  private RatingRepository ratingRepository;
+	@Autowired
+	private ArticleRepository articleRepository;
 
-  @Transactional(readOnly = true)
-  public DoubleLinkedList<Article> findAll() {
-    DoubleLinkedList<Article> articleList = new DoubleLinkedList<Article>();
-    articleList.addAll(articleRepository.findAll());
-    return articleList;
-  }
+	@Autowired
+	private UserRepository userRepository;
 
-  @Transactional(readOnly = true)
-  public Article findById(Long id) {
-    Optional<Article> opt = articleRepository.findById(id);
-    Article article = opt.orElseThrow(
-      () -> new EntityNotFoundException("Entity not found")
-    );
-    return article;
-  }
+	@Autowired
+	private RatingRepository ratingRepository;
 
-  @Transactional
-  public Article save(Article articleDto) {
-    Article articleDb = articleRepository.save(articleDto);
-    return articleDb;
-  }
+	@Autowired
+	private FileUtil fileUtil;
 
-  @Transactional
-  public Article update(Long id, Article articleDto) {
-    Optional<Article> articleOpt = articleRepository.findById(id);
-    Article articleEntity = articleOpt.orElseThrow(
-      () -> new EntityNotFoundException("Entity not found")
-    );
+	@Transactional(readOnly = true)
+	public DoubleLinkedList<Article> findAll() {
+		DoubleLinkedList<Article> articleList = new DoubleLinkedList<Article>();
+		articleList.addAll(articleRepository.findAll());
+		return articleList;
+	}
 
-    articleEntity.copy(articleDto);
-    
-    // Relacionamento com usuário
-    articleEntity.setUser(
-    	userRepository.findById(articleDto.getUser().getId())
-    		.orElseThrow(() -> new EntityNotFoundException("Entity not found"))
-    );
-    
-    // Relacionamento com avaliação
-    articleEntity.setRatings(
-    	articleDto.getRatings().stream().map(rating -> {
-    		return ratingRepository.findById(rating.getId()).orElseThrow(() -> new EntityNotFoundException("Entity not found"));
-    	}).collect(Collectors.toSet())
-    );
+	@Transactional(readOnly = true)
+	public Article findById(Long id) {
+		Optional<Article> opt = articleRepository.findById(id);
+		Article article = opt.orElseThrow(
+				() -> new EntityNotFoundException("Entity not found")
+				);
+		return article;
+	}
 
-    return articleRepository.save(articleEntity);
-  }
+	@Transactional
+	public Article save(Article articleDto, MultipartFile attachment) {
+		
+		Article articleEntity = articleRepository.save(articleDto);
 
-  @Transactional
-  public void deleteArticle(Long id) {
-    Optional<Article> articleOpt = articleRepository.findById(id);
-    Article articleEntity = articleOpt.orElseThrow(
-      () -> new EntityNotFoundException("Entity not found")
-    );
+		if(attachment != null) {
+			saveArticleAttachment(articleEntity, attachment);
+			articleEntity = articleRepository.save(articleDto);
+		}
 
-    articleRepository.delete(articleEntity);
-  }
+		return articleEntity;
+	}
+
+	@Transactional
+	public Article update(Long id, Article articleDto, MultipartFile attachment) {
+		Optional<Article> articleOpt = articleRepository.findById(id);
+		Article articleEntity = articleOpt.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+
+		articleEntity.copy(articleDto);
+
+		// Relacionamento com usuário
+		articleEntity.setUser(
+			userRepository.findById(articleDto.getUser().getId())
+				.orElseThrow(() -> new EntityNotFoundException("Entity not found"))
+		);
+
+		// Relacionamento com avaliação
+		articleEntity.setRatings(
+			articleDto.getRatings().stream().map(rating -> {
+				return ratingRepository.findById(rating.getId()).orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+			}).collect(Collectors.toSet())
+		);
+		
+		if(attachment != null) {
+			saveArticleAttachment(articleEntity, attachment);
+		}
+
+		return articleRepository.save(articleEntity);
+	}
+
+	@Transactional
+	public void deleteArticle(Long id) {
+		Optional<Article> articleOpt = articleRepository.findById(id);
+		Article articleEntity = articleOpt.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+
+		deleteAttachment(articleEntity);
+		articleRepository.delete(articleEntity);
+	}
+	
+	public void deleteArticleAttachment(Long id) {
+		Optional<Article> articleOpt = articleRepository.findById(id);
+		Article articleEntity = articleOpt.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		
+		deleteAttachment(articleEntity);
+	}
+	
+	private void saveArticleAttachment(Article entity, MultipartFile attachment) {
+		String path = "article/" +  entity.getId() + "/";
+		String fileName = "attachment." + FileUtil.getFileExtension(attachment.getOriginalFilename());
+
+		// Remove o arquivo que já está salvo caso ele exista
+		if(entity.getAttachment() != null) {
+			FileUtil.removeFile(entity.getAttachment());
+		}
+
+		fileUtil.saveFile(path, fileName, attachment);
+		entity.setAttachment(path + fileName);
+	}
+
+	private void deleteAttachment(Article entity) {
+		if(FileUtil.removeFile(entity.getAttachment())) {			
+			entity.setAttachment(null);
+		}
+	}
 }
